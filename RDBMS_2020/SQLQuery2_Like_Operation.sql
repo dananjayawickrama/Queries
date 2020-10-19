0b892a1991bb6feed2039780de8326701164d763
dananjayawickrama <dananjayawickrama95@gmail.com>
2020-10-19 15:22:07 +0530

    Add SQL2_like_operation file

---
+++ RDBMS_2020/SQLQuery2_Like_Operation.sql
@@ -0,0 +1,32 @@
+--//Like Operation
+
+-- Table column -> id(PK),name,gpa,ProID
+
+
+SELECT *
+FROM LIKE_operator_Table_1;
+
+
+--Find students whose name starts  with a ‘K’
+
+select id,name
+from LIKE_operator_Table_1
+where name like 'K%';
+
+-- Student name that have "ad" in any position:
+
+select id,name
+from LIKE_operator_Table_1
+where name like '%ad%';
+
+--Student name that have "a" in the second position:
+
+select id,name
+from LIKE_operator_Table_1
+where name like '_a%';
+
+--Student  that does NOT start with "D":
+
+select id,name
+from LIKE_operator_Table_1
+where name not like 'D%';
