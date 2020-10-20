f2d428886b8060925ba702122fae908751811e7f
dananjayawickrama <dananjayawickrama95@gmail.com>
2020-10-20 09:35:53 +0530

    Upload the BETWEEN SQL File

---
+++ RDBMS_2020/SQLQuery5_BETWEEN_operator.sql
@@ -0,0 +1,20 @@
+--//Between SQL
+
+--// Print 1 and 3 items also
+SELECT *
+FROM Table_1
+WHERE Number BETWEEN 1 AND 3;
+
+
+
+SELECT *
+FROM Table_1
+WHERE Number NOT BETWEEN 1 AND 3;
+
+
+SELECT *
+FROM Table_1;
+
+select count(Number)
+from Table_1
+where mydate between '06-10-2020' and '06-10-2020';
