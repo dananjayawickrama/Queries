a21ee1ca6cd5f5c6fdc19f459b56c3b9335fd13b
dananjayawickrama <dananjayawickrama95@gmail.com>
2020-10-20 10:23:10 +0530

    Upload the ASc_DESC_Order SQL File

---
+++ RDBMS_2020/SQLQuery6_ASC_DESC_Order_operator.sql
@@ -0,0 +1,26 @@
+--//ORDER BY keyword
+
+--01.ascending order
+
+
+--Table->Id(pk),name,gpa,batch
+
+SELECT id,name,gpa
+FROM LIKE_operator_Table_1
+ORDER By gpa;
+
+
+SELECT id,name,gpa
+FROM LIKE_operator_Table_1
+ORDER By gpa ASC;
+
+SELECT id,name,gpa
+FROM LIKE_operator_Table_1
+ORDER By gpa;
+
+
+
+--02.descending  order
+SELECT id,name,gpa
+FROM LIKE_operator_Table_1
+ORDER By gpa DESC;
