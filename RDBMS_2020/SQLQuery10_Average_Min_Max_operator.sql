a16acd49fd81e0037a780dc8726bd15284e13b18
dananjayawickrama <dananjayawickrama95@gmail.com>
2020-10-24 12:29:06 +0530

    Upload AVG_MIN_MIN OPERATIONS SQl file

---
+++ RDBMS_2020/SQLQuery10_Average_Min_Max_operator.sql
@@ -0,0 +1,8 @@
+
+--// average, minimum, maximum 
+
+--// Find the average, minimum, maximum gpa of students?
+
+select AVG(gpa) as AVERAGE,MAX(gpa) as MAXIMUM,MIN(gpa) as MINIMUM
+from Table_1_Student;  
+
